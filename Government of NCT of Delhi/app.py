983f7cbd5a95adcc4abc36fbac5228b2243b95c7
debaraja-394 <140910432+debaraja-394@users.noreply.github.com>
2024-08-29 20:06:21 +0000

    add_doc_included

--- Government of NCT of Delhi/app.py
+++ Government of NCT of Delhi/app.py
@@ -50,6 +50,28 @@ def landing():
     return render_template('index.html')
 
 
+@app.route('/user_login', methods=['POST', 'GET'])
+def user_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Find the user by username
+        user = users_collection.find_one({'username': username})
+
+        if user:
+            # Compare the entered password with the stored hashed password
+            if bcrypt.check_password_hash(user['password'], password):
+                return redirect('/appointment')
+            else:
+                return 'Wrong password'
+        
+        return 'User not found'
+
+    return render_template('user_login.html')
+
+
+
 @app.route('/user_register',methods=['GET','POST'])
 def user_register():
     if request.method=='POST':
@@ -57,27 +79,80 @@ def user_register():
         number=request.form['phone']
         email= request.form['email']
         user_name = request.form['username']
-        existing_user = users_collection.find_one({'$or': [{'username': user_name}, {'email': email}]})
-        if existing_user:
-            return 'Username or email already exists'
+        # existing_user = users_collection.find_one({'$or': [{'username': user_name}, {'email': email}]})
+        # if existing_user:
+        #     return 'Username or email already exists'
         pa = request.form['password']
         password=bcrypt.generate_password_hash(pa).decode('utf-8')
         user_data={
             'name':name,
             'username':user_name,
+            'email':email,
             'number':number,
             'password':password
         }
         users_collection.insert_one(user_data)
-        return render_template('user login.html')
+        return redirect('/user_login')
     return render_template('user register.html')
 
 
+@app.route('/appointment', methods=['POST', 'GET'])
+def appointment():
+    if request.method == 'POST':
+        # Extract form data
+        name = request.form['name']
+        number = request.form['number']
+        email = request.form['email']
+        address = request.form['Address']
+        appointment_date = request.form['dat']
+        time_slot = request.form['timeSlot']
+        speciality = request.form['diseaseInput']
+        disease_description = request.form['diseaseDescription']
+        appointment_data = {
+            'name': name,
+            'number': number,
+            'email': email,
+            'address': address,
+            'appointment_date': appointment_date,
+            'time_slot': time_slot,
+            'speciality': speciality,
+            'disease_description': disease_description
+        }
+        appointment_collection.insert_one(appointment_data)
 
+        # After saving or processing, redirect or render a success page
+        return "Appointment Sucessfull"
+    # If GET request, just render the appointment form
+    return render_template('appointment.html')
 
+#Adding Doctor
 
+@app.route('/add_doc',methods=['POST','GET'])
+def doctor_register():
+    if request.method=='POST':
+        name=request.form['doctor_name']
+        specialization=request.form['specialization']
+        qualification=request.form['qualification']
+        email=request.form['email']
+        username=request.form['username']
+        password=request.form['password']
+        hash_password=bcrypt.generate_password_hash(password).decode('utf-8')
+        phone=request.form['phone']
+        aadhar=request.form['aadhaar']
 
-
+        doctor_data={
+            'name':name,
+            'specialization':specialization,
+            'qualification':qualification,
+            'email':email,
+            'username':username,
+            'password':hash_password,
+            'phone':phone,
+            'aadhar':aadhar
+        }
+        doctors_collection.insert_one(doctor_data)
+        return render_template('add doc.html')
+    return render_template('add doc.html')
 
 
 
