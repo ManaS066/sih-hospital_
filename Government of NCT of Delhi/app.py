1c1a88164cc88122ce3fb273565e1981dad6920b
Manas Pradhan <manasranjanpradhan2004@gmail.com>
2024-08-31 20:23:59 +0530

    test

--- Government of NCT of Delhi/app.py
+++ Government of NCT of Delhi/app.py
@@ -396,5 +396,6 @@ def check_hospital():
     
     return render_template('superadmin_hospital_status.html')
 #where is the change
+#show
 if __name__ == '__main__':
     app.run( port=8000,debug=True)
