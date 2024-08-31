1951dee656b529c12dd96ae2ee9614f14d40231b
Manas Pradhan <manasranjanpradhan2004@gmail.com>
2024-08-31 20:22:07 +0530

    test comment

--- Government of NCT of Delhi/app.py
+++ Government of NCT of Delhi/app.py
@@ -395,6 +395,6 @@ def check_hospital():
             return "No hospital found"
     
     return render_template('superadmin_hospital_status.html')
-
+#where is the change
 if __name__ == '__main__':
     app.run( port=8000,debug=True)
