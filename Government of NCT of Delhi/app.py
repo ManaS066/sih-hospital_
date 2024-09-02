2a9c1a1fb3482edbc134808fbcd541555bdf4667
Manas Pradhan <manasranjanpradhan2004@gmail.com>
2024-09-03 00:45:14 +0530

    inventory testing

--- Government of NCT of Delhi/app.py
+++ Government of NCT of Delhi/app.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask_bcrypt import Bcrypt
 from datetime import datetime, timedelta
 from pymongo.server_api import ServerApi
@@ -396,7 +397,7 @@ def admin():
                                vacent_general=vacent_general,vacent_icu=vacent_icu,vacent_ventilator=vacent_ventilator,hospital_name=hospital_name,nurses=nurses,staff=staff)
     else:
         return redirect('/admin/add_detail')
-
+    
 @app.route("/admin/contact-us")
 @login_required('admin')
 def admin_contact_us():
@@ -550,6 +551,9 @@ def doc_login():
     # Replace with your login template
     return render_template('doctor login.html')
 
+@app.route('/stock_detail')
+def detail():
+    return render_template('inv_stock_product.html')
 
 @app.route('/doctor_app', methods=["POST", "GET"])
 @login_required('doc')
