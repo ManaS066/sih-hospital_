5c72362de45fe7adaccf4cdfcdd6661ad6c7a49a
debaraja-394 <debabrat827@gmail.com>
2024-09-06 20:39:36 +0530

    Added alert message for logout in the homepage

--- Government of NCT of Delhi/log_out.py
+++ Government of NCT of Delhi/log_out.py
@@ -1,25 +1,29 @@
-from flask import session, redirect,Blueprint
+from flask import session, redirect,Blueprint,flash
 
 logout_bp=Blueprint('logout_bp',__name__)
 
 @logout_bp.route('/user_logout')
 def user_logout():
     session.clear()
+    flash('You have logged out', 'success')
     return redirect('/')
 
 
 @logout_bp.route('/admin_logout')
 def admin_logout():
     session.clear()
+    flash('You have logged out', 'success')
     return redirect('/')
 
 
 @logout_bp.route('/superadmin_logout')
 def sueperadmin_logout():
     session.clear()
+    # flash('You have logged out', 'success')
     return redirect('/superadmin')
 
 @logout_bp.route('/doctor_logout')
 def doctor_logout():
     session.clear()
+    flash('You have logged out', 'success')
     return redirect('/')
